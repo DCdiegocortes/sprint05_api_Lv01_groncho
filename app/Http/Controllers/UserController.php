6a5900a99244efb728cd1fb7482c45734458a464
Diego Cortes <dc.diegocortes@gmail.com>
2026-09-22 12:55:49 +0200

    feat: implement admin user management endpoints (GET/DELETE /users)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,4 +13,8 @@ Route::middleware('auth:api')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::put('/profile', [ProfileController::class, 'update']);
+
+    Route::middleware('admin')->group(function () {
+        Route::apiResource('users', UserController::class)->only(['index', 'show', 'destroy']);
+    });
 });
