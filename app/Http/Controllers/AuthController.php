e9f07e1bb3434176c36c60700833b619ca835b78
Diego Cortes <dc.diegocortes@gmail.com>
2026-09-22 11:08:38 +0200

    feat: implement POST /register endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        return response()->json($user, 201);
+    }
+}
